278620fa809521708d5f9e89bee80eeff534a154
koushik K M <koushik@avesorg.com>
2017-07-09 17:35:30 +0530

    webpages added

--- hytte/settings.py
+++ hytte/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '/static/'),
+    os.path.join(BASE_DIR, '/static/'),
     """[
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
